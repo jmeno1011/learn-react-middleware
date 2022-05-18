2c22f42b821c65e36921402f822bcc2ba17de16e
jmeno1011 <jwhltn8282@gmail.com>
2022-05-19 00:56:40 +0900

    middleware도입 준비 끝

---
+++ src/modules/counter.js
@@ -0,0 +1,20 @@
+import { handleActions } from "redux-actions";
+import { createAction } from "redux-actions";
+
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initialState = 0;
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
+
+export default counter;
